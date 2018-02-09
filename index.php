250c0ec865f4d3ad630ecbcd0cfe0d1ac76e158c
Dean Day <dean@greenlightgames.co.uk>
2018-02-09 10:18:52 +0000

    Removed DB SQL Updated SQL Query on Index Test

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 	echo "Hello there, stranger!";
 
 // mysqli
-$mysqli = new mysqli("35.197.200.94", "hack_dbadmin", "gm256mwE5pmdpMw0bAN", "mysql");
-$result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM mysql");
+$mysqli = new mysqli("35.197.200.94", "hack_dbadmin", "gm256mwE5pmdpMw0bAN", "PHPHackGame");
+$result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM Hackers");
 $row = $result->fetch_assoc();
 echo htmlentities($row['_message']);
 
