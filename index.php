aad2918f481c8b2b80b47a19295410e5dfb6df5e
Dean Day <dean@greenlightgames.co.uk>
2018-02-09 10:22:20 +0000

    More SQL Tests

--- index.php
+++ index.php
@@ -3,8 +3,25 @@
 
 // mysqli
 $mysqli = new mysqli("35.197.200.94", "hack_dbadmin", "gm256mwE5pmdpMw0bAN", "PHPHackGame");
-$result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM Hackers");
-$row = $result->fetch_assoc();
-echo htmlentities($row['_message']);
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+$query = "SELECT HackerID, EmailAd FROM Hackers";
+$result = $mysqli->query($query);
+
+/* numeric array */
+$row = $result->fetch_array(MYSQLI_NUM);
+printf ("%s (%s)\n", $row[0], $row[1]);
+
+/* associative array */
+$row = $result->fetch_array(MYSQLI_ASSOC);
+printf ("%s (%s)\n", $row["HackerID"], $row["EmailAd"]);
+
+/* associative and numeric array */
+$row = $result->fetch_array(MYSQLI_BOTH);
+printf ("%s (%s)\n", $row[0], $row["EmailAd"]);
 
 ?>
